8ca3b0161084bcc8b3eec3bb9bc9492d890ba51c
JungTag <dbsdudxor121@naver.com>
2020-05-24 01:13:02 +0900

    알고리즘 스터디3회차 - 정렬2

--- Sorting/11650.py
+++ Sorting/11650.py
@@ -1,11 +1,11 @@
 import sys
-sys.setrecursionlimit(10**8)
 
 n = int(input())
 input_list = [sys.stdin.readline().rstrip() for i in range(n)]
 li =[]
 for x in input_list:
-    li.append(list(map(int, x.split())))
+    li.append(list(map(int, x.split()))) #좌표를 리스트로 만들어 li에 append
+
 
 def quick_sort(li, first, last):
     if first >= last: return
@@ -33,16 +33,19 @@ for x, y in li:
 1) 리스트를 만들어 병합하는 방식의 not in-place한 방식의 quick sort는 
 너무 많은 재귀호출과 메모리 필요, 따라서 in-place한 방식의 quick sort사용
 2) x와 y를 나눠서 정렬해야 된다는 생각 때문에 시간을 버림
-리스트 자료형은 원소 순서짝대로 서로 비교하기 때문에 알아서 정렬됨
+리스트 자료형은 원소 순서짝대로 서로 비교(line 15, 17)하기 때문에 알아서 정렬됨
 
 의문:
 for x, y in li:
     print(x, y) <-- 이건 되는데
-for x in li:
+    
+for x in li: print
     print(x[0], x[1]) <-- 이건 왜 안 될까?
 
 깨달은 점:
 - 많은 입력을 요할 땐 sys.stdin을 사용하자
 - not-in place한 알고리즘 사용을 자제하자
-'''
+''' 
+
+
         
