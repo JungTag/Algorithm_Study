829da165345e3b42c6833eb093bfc989422cecdf
JungTag <dbsdudxor121@naver.com>
2021-01-12 20:43:29 +0900

    1/12 TSP

--- 2293(동전1).py
+++ 2293(동전1).py
@@ -3,6 +3,7 @@ import sys
 n, k = map(int, sys.stdin.readline().split())
 coins = []
 dp = [0 for _ in range(k+1)] 
+dp[0] = 1
 
 for _ in range(n):
     coins.append(int(sys.stdin.readline().strip()))
@@ -10,7 +11,6 @@ for _ in range(n):
 coins = list(set(coins)) # 중복 제거
 
 for coin in coins:
-    dp[0] = 1
     for i in range(1, k+1):
         if i - coin >= 0:
             dp[i] += dp[i-coin]
