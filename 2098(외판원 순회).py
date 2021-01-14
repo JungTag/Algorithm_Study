e8dae7939ab1f47b33a513e6ba662ef804d4b192
JungTag <dbsdudxor121@naver.com>
2021-01-14 22:44:07 +0900

    1/14 dp 1

--- 2098(외판원 순회).py
+++ 2098(외판원 순회).py
@@ -3,8 +3,8 @@ sys.setrecursionlimit(10**9)
 
 n = int(input())
 w = []
-dp = [[0 for _ in range(n)] for i in range(2**n)]
 INF = float('inf')
+dp = [[INF for _ in range(n)] for i in range(2**n)]
 
 for _ in range(n):
     raw = [int(x) for x in sys.stdin.readline().split()]
@@ -14,15 +14,13 @@ def travel(visited, cur):
     visited = visited | (1 << cur) # 방문한 도시를 기록
 
     if visited == (1 << n) - 1: # 모든 도시를 방문한 경우
-        if w[cur][0] != 0: # 출발 도시로 갈 수 있는 경로가 있는 경우 -> 사이클 때문에 어느 도시에서 출발하든 최소 비용은 같다. 따라서 출발 도시는 0번 도시로 한다. 
+        if w[cur][0] != 0: # 출발 도시로 갈 수 있는 경로가 있는 경우 
             return w[cur][0]
         else:
             return INF
     
-    if dp[visited][cur] != 0: # dp[현재까지 방문한 도시들][현재 도시] -> 현재 도시에서 출발 도시까지 가는 데 필요한 비용 
+    if dp[visited][cur] != INF: # dp[현재까지 방문한 도시들][현재 도시] -> 현재 도시에서 출발 도시까지 돌아가는 데 필요한 비용 
         return dp[visited][cur]
-    else:
-        dp[visited][cur] = INF
     
     for u in range(n):
         if u != cur and (visited & (1 << u)) == 0 and w[cur][u] != 0: # 1) 현재 도시가 아니고 2) 아직 방문하지 않았으며 3) 경로가 있는 도시를 찾는다.
@@ -32,7 +30,7 @@ def travel(visited, cur):
 
     return dp[visited][cur]
 
-print(travel(0, 0))
+print(travel(0, 0)) # 사이클 때문에 어느 도시에서 출발하든 최소 비용은 같다. 따라서 출발 도시는 0번 도시로 한다. 
     
 
 
